a6094ff56f011df4d639c2a0ffa0e646270b5807
ParkYeoungJun <dudwns4971@gmail.com>
2016-09-21 01:51:37 +0900

    no message

--- Raspberry/src/frontdoor/NotePanel.java
+++ Raspberry/src/frontdoor/NotePanel.java
@@ -8,6 +8,7 @@ import java.awt.Image;
 import java.awt.TextArea;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.GrayFilter;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -29,8 +30,9 @@ public class NotePanel extends JPanel
 	public NotePanel(Dimension fulldim)
 	{
 		this.fulldim = fulldim;
-		this.setBounds(0, fulldim.height/15, fulldim.width/3*2, fulldim.height-fulldim.height/15);
+		this.setBounds(0, fulldim.height/15, fulldim.width/2, fulldim.height-fulldim.height/15);
 		this.setLayout(null);
+		this.setOpaque(false);
 				
 		Image tempimg = new ImageIcon("post.PNG").getImage();
 		
@@ -51,14 +53,17 @@ public class NotePanel extends JPanel
 		//	note.setBounds(0, 0, fulldim.width/3*2, fulldim.height-fulldim.height/15);
 		note.setEditable(false);
 		note.setFont(new Font(null, Font.BOLD, 20));
-		note.setBackground(Color.WHITE);
+//		note.setBackground(Color.WHITE);
 		note.setLineWrap(true);
 		note.setOpaque(false);
+		note.setBorder(BorderFactory.createEmptyBorder());
+		note.setForeground(Color.WHITE);
 		
 		scroll = new JScrollPane(note, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scroll.setBounds(0, 0, fulldim.width/3*2, fulldim.height-fulldim.height/15);
 //		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scroll.setOpaque(false);
+		scroll.getViewport().setOpaque(false);
 		this.add(scroll);
 		
 		updateThread up = new updateThread();
@@ -101,6 +106,7 @@ public class NotePanel extends JPanel
 		else
 		{
 			note.setText("");
+			note.setText("≥Î∆Æ\n\n\n");
 			
 			for(int i = 0 ; i < notedata.size() ; ++i)
 			{	
